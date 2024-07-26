66fa87dd5cd634887eaff89099d2212809e795ba
Aaron Cooley <aaron.cooley@progleasing.com>
2024-07-26 14:08:16 -0600

    ready for beta testing

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # retailcal
 
-FIXME: description
+retailcal is a library for converting between standard (Gregorian) calendars and
+various retail calendars such as the ones used by the National Retail
+Foundation. It supports the 4-5-4, 4-4-5, and 5-4-4 formats, named for the
+number of retail weeks per retail month, repeated in sequence four times during
+the year. It also automatically detects when a retail year needs to be 53
+retail weeks.
 
 ## Installation
 
@@ -8,29 +13,154 @@ Download from http://example.com/FIXME.
 
 ## Usage
 
-FIXME: explanation
+The primary use case is to determine the retail year, retail week, and retail
+month of a given date in the Gregorian calendar. The primary function to do this
+is `(->retail-date <date>)`. This takes either a java.time.LocalDate or a
+string that can be coerced to java.time.LocalDate, and returns a map containing
+the following fields:
 
-    $ java -jar retailcal-0.1.0-standalone.jar [args]
+* `:calendar-date`
+* `:retail-year`
+* `:retail-quarter-of-year`
+* `:retail-month-of-year`
+* `:retail-month-of-quarter`
+* `:retail-week-of-year`
+* `:retail-week-of-quarter`
+* `:retail-week-of-month`
+* `:retail-day-of-year`
+* `:retail-day-of-quarter`
+* `:retail-day-of-month`
+* `:retail-day-of-week`
+* `:retail-year-start-date`
+* `:retail-year-end-date`
+* `:retail-quarter-start-date`
+* `:retail-quarter-end-date`
+* `:retail-month-start-date`
+* `:retail-month-end-date`
+* `:retail-week-start-date`
+* `:retail-week-end-date`
+
+The algorithm used to determine the structure of a retail year is as follows:
+
+* Find `:retail-year-start-date`, or the first day of the retail year, by
+looking for the calendar date of the first `:start-day` in the `:start-month`
+for the calendar year of `<date>`. If the day of the month for that day is <= 4
+(e.g. February 1-4), that date will be the first day of the `:retail-year`. If
+the calendar date is >4 (e.g. February 5-7), `:retail-year-start-date` will
+be one week before that date.
+
+* Based on `:retail-year-start-date`, follow the `:calendar-style` by assigning
+the correct number of weeks to each month. See details on the `:calendar-style`
+below.
+
+* Using the above year, quarter, month, and week mappings, determine the start
+and end dates of each period.
 
 ## Options
 
-FIXME: listing of options this app accepts.
+In addition to the mandatory date, several options are available to specify the
+structure of the retail calendar you are using.
+
+`:calendar-style` must be one of `"454"`, `"544"`, or `"445"` (default is `"454"`),
+and represents the structure of retail weeks per retail month in a given retail
+quarter. As an example, `"454"` style means the first retail month of each retail
+quarter contains 4 retail weeks, the second contains 5, and the third contains
+4.
+
+`:start-month` specifies the first month of the retail year (default is
+`:february`). The `:retail-year` will match the calendar year of
+`:start-month`, so the a retail calendar that starts on February 4, 2024 will
+be `:retail-year` 2024 (including January 1-February 1, 2025).
+
+`:start-day` specifies the first day of of the retail week
+(default is `:sunday`).
 
 ## Examples
 
-...
+`(->retail-date "2024-05-05")`
+
+returns the following map:
+
+```
+{:retail-day-of-quarter 1,
+ :retail-week-end-date #object[java.time.LocalDate 0x3152c06 "2024-05-11"],
+ :retail-week-of-month 1,
+ :retail-week-of-quarter 1,
+ :calendar-date #object[java.time.LocalDate 0x3f2cf455 "2024-05-05"],
+ :retail-day-of-year 92,
+ :retail-quarter-start-date
+ #object[java.time.LocalDate 0x4a3aa368 "2024-05-05"],
+ :retail-month-start-date #object[java.time.LocalDate 0x47f009c7 "2024-05-05"],
+ :retail-year-start-date #object[java.time.LocalDate 0x3bc47e66 "2024-02-04"],
+ :retail-year 2024,
+ :retail-day-of-month 1,
+ :retail-week-start-date #object[java.time.LocalDate 0xeb97117 "2024-05-05"],
+ :retail-year-end-date #object[java.time.LocalDate 0x6ca5842 "2025-02-01"],
+ :retail-quarter-end-date #object[java.time.LocalDate 0x45c74fcb "2024-08-03"],
+ :retail-month-of-year 4,
+ :retail-quarter-of-year 2,
+ :retail-week-of-year 14,
+ :retail-month-end-date #object[java.time.LocalDate 0x2cf6a5e2 "2024-06-01"],
+ :retail-month-of-quarter 1,
+ :retail-day-of-week 1}
+```
+
+`(retail-calendar "2020-01-01" "2020-01-31" :calendar-style "544" :start-month :january)`
+
+will return a sequence of maps, one for each day between January 1 and 31, 2020.
+
+`(retail-calendar-dataset "2030-01-01" "2030-12-31" :start-day :saturday)`
+
+will return a `scicloj.tablecloth` dataset with one column per field and one row per calendar date.
+
+`(write-retail-calendar! "monday-445.csv" "1970-01-01" "2099-12-31" :calendar-style "445" :start-day :monday)`
+
+will write a .csv file to disk containing one row for each date from January 1, 1970 to December 31, 2099.
+
+`(retail-year->calendar 2020)`
+
+will return a `scicloj.tablecloth` dataset with one row for every day in retail year 2020
+
+`(printable-calendar :retail-year 2021 :calendar-style "544")` (NOTE: experimental)
+
+will create a human-readable calendar that can be used to check dates, convert to a print-ready calendar, etc., per the below:
+
+```
+_unnamed [52 8]:
+
+|      rows/cols | SUN | MON | TUE | WED | THU | FRI | SAT |
+|----------------|----:|----:|----:|----:|----:|----:|----:|
+|   [2021 1 1 1] |  31 |   1 |   2 |   3 |   4 |   5 |   6 |
+|   [2021 1 1 2] |   7 |   8 |   9 |  10 |  11 |  12 |  13 |
+|   [2021 1 1 3] |  14 |  15 |  16 |  17 |  18 |  19 |  20 |
+|   [2021 1 1 4] |  21 |  22 |  23 |  24 |  25 |  26 |  27 |
+|   [2021 1 1 5] |  28 |   1 |   2 |   3 |   4 |   5 |   6 |
+|   [2021 1 2 6] |   7 |   8 |   9 |  10 |  11 |  12 |  13 |
+|   [2021 1 2 7] |  14 |  15 |  16 |  17 |  18 |  19 |  20 |
+|   [2021 1 2 8] |  21 |  22 |  23 |  24 |  25 |  26 |  27 |
+|   [2021 1 2 9] |  28 |  29 |  30 |  31 |   1 |   2 |   3 |
+|  [2021 1 3 10] |   4 |   5 |   6 |   7 |   8 |   9 |  10 |
+|            ... | ... | ... | ... | ... | ... | ... | ... |
+| [2021 4 10 42] |  14 |  15 |  16 |  17 |  18 |  19 |  20 |
+| [2021 4 10 43] |  21 |  22 |  23 |  24 |  25 |  26 |  27 |
+| [2021 4 10 44] |  28 |  29 |  30 |   1 |   2 |   3 |   4 |
+| [2021 4 11 45] |   5 |   6 |   7 |   8 |   9 |  10 |  11 |
+| [2021 4 11 46] |  12 |  13 |  14 |  15 |  16 |  17 |  18 |
+| [2021 4 11 47] |  19 |  20 |  21 |  22 |  23 |  24 |  25 |
+| [2021 4 11 48] |  26 |  27 |  28 |  29 |  30 |  31 |   1 |
+| [2021 4 12 49] |   2 |   3 |   4 |   5 |   6 |   7 |   8 |
+| [2021 4 12 50] |   9 |  10 |  11 |  12 |  13 |  14 |  15 |
+| [2021 4 12 51] |  16 |  17 |  18 |  19 |  20 |  21 |  22 |
+| [2021 4 12 52] |  23 |  24 |  25 |  26 |  27 |  28 |  29 |
+```
 
 ### Bugs
 
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
+Please submit bug reports or feature requests to https://github.com/Prometheus77/retailcal/
 
 ## License
 
-Copyright © 2024 FIXME
+Copyright © 2024
 
 This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0 which is available at
--- src/retailcal/calendar.clj
+++ src/retailcal/calendar.clj
@@ -1,6 +1,7 @@
 (ns retailcal.calendar
   (:require [java-time.api :as jt]
-            [tablecloth.api :as tc]))
+            [tablecloth.api :as tc])
+  (:import [java.time.format DateTimeFormatter]))
 
 (defn- parse-date
   "Parses the input date, which can be a string or a java.time.LocalDate instance."
@@ -10,26 +11,7 @@
     (string? date) (jt/local-date date)
     :else (throw (IllegalArgumentException. "Invalid date format"))))
 
-(defn- day-value
-  "Returns the numeric value of the given day keyword."
-  [day]
-  (case day
-    :sunday 7
-    :monday 1
-    :tuesday 2
-    :wednesday 3
-    :thursday 4
-    :friday 5
-    :saturday 6))
-
-(defn- retail-day-of-week
-  "Calculates the retail day of the week given a date and the start day of the week."
-  [date start-day]
-  (let [weekday (jt/as date :day-of-week)
-        start-day-value (day-value start-day)]
-    (-> (- weekday start-day-value) (+ 7) (mod 7) inc)))
-
-(defn- retail-year-start-date
+(defn- calc-year-start-date
   "Finds the start date of the retail year given the year, start month, and start day."
   [year start-month start-day]
   (let [first-day-of-month (jt/local-date year (jt/month start-month) 1)
@@ -38,126 +20,266 @@
       first-start-day
       (jt/minus first-start-day (jt/days 7)))))
 
-(defn- find-retail-year
-  "Determines the retail year for a given date, start month, and start day."
-  [date start-month start-day]
-  (let [year (jt/as date :year)
-        candidates (map #(retail-year-start-date % start-month start-day)
-                        [(dec year) year (inc year) (+ 2 year)])
-        candidate (last (filter #(jt/>= date %) candidates))]
-    (jt/as candidate :year)))
-
-(defn- retail-month-and-week
-  "Calculates the retail month and week within the month given the calendar style and week of the year."
-  [calendar-style week-of-year]
-  (let [quarter-pattern (condp = calendar-style
-                          "454" [4 5 4]
-                          "445" [4 4 5]
-                          "544" [5 4 4]
-                          (throw (IllegalArgumentException. (str "Invalid calendar style: " calendar-style))))
-        first-weeks (->> quarter-pattern (repeat 4) flatten (reductions + 1) butlast)
-        month-start-week (last (take-while #(<= % week-of-year) first-weeks))
-        month-of-year-index (.indexOf first-weeks month-start-week)
-        month-of-year (inc month-of-year-index)
-        week-of-month (inc (- week-of-year (nth first-weeks month-of-year-index)))
-        quarter-of-year (-> month-of-year dec (quot 3) inc)
-        quarter-start-week (nth first-weeks (* (dec quarter-of-year) 3))
-        week-of-quarter (inc (- week-of-year quarter-start-week))]
-    {:month-of-year month-of-year
-     :quarter-of-year quarter-of-year
-     :month-of-quarter (-> month-of-year dec (mod 3) inc)
-     :week-of-quarter week-of-quarter
-     :week-of-month week-of-month}))
-
 (defn ->retail-date
-  "Converts a given date into retail calendar fields."
-  ([date & {:keys [calendar-style start-month start-day]
-            :or {calendar-style "454"
-                 start-month :february
-                 start-day :sunday}}]
-   (let [date (parse-date date)
-         year (find-retail-year date start-month start-day)
-         year-start-date
-         (retail-year-start-date year start-month start-day)
-         week-of-year (inc (quot (jt/time-between year-start-date date :days) 7))
-         {:keys [month-of-year week-of-month quarter-of-year month-of-quarter week-of-quarter]}
-         (retail-month-and-week calendar-style week-of-year)
-         day-of-week (retail-day-of-week date start-day)
-         day-of-month (-> week-of-month
-                          dec
-                          (* 7)
-                          (+ day-of-week))]
-     {:calendar-date (str date)
-      :retail-year year
-      :retail-quarter-of-year quarter-of-year
-      :retail-month-of-year month-of-year
-      :retail-week-of-year week-of-year
-      :retail-day-of-year (inc (jt/time-between year-start-date date :days))
-      :retail-month-of-quarter month-of-quarter
-      :retail-week-of-quarter week-of-quarter
-      :retail-week-of-month week-of-month
-      :retail-day-of-quarter (-> week-of-quarter dec (* 7) (+ day-of-week))
-      :retail-day-of-month day-of-month
-      :retail-day-of-week day-of-week})))
+  "
+  Converts a given date into retail calendar fields using specified options. Defaults
+  to the National Retail Federation's 4-5-4 calendar, described at:
+  https://nrf.com/resources/4-5-4-calendar
 
-(defn ->retail-dates
-  "Generates retail dates for a vector of dates or a range defined by start-date and end-date."
-  ([dates & {:keys [calendar-style start-month start-day]
-             :or {calendar-style "454"
-                  start-month :february
-                  start-day :sunday}}]
-   (let [date-seq (map parse-date dates)]
-     (map #(->retail-date % :calendar-style calendar-style :start-month start-month :start-day start-day) date-seq))))
+  Options:
 
-(defn ->retail-date-range
-  "Generates retail dates for a range defined by start-date and end-date."
+  :calendar-style
+  Must be one of \"454\" \"445\" \"544\" (default \"454\").
+  Specifies the number of weeks for each month in a given retail quarter.
+  \"454\" indicates that each quarter consists of a 4-week month, followed
+  by a 5-week month, followed by a 4-week month.
+
+  :start-month
+  Must be a month name as a keyword. Default is :february. Indicates the
+  calendar month which constitues the first month in the retail year.
+
+  :start-day
+  Must be a day of the week as a keyword. Default is :sunday. Indicates
+  the day on which each retail week begins.
+
+  Example:
+
+  (->retail-date \"2023-10-01\" :calendar-style \"445\" :start-month :february :start-day :monday)
+
+  (->retail-date \"2025-12-31\")
+
+  Returns a map containing the following fields:
+  :calendar-date
+  :retail-year
+  :retail-year-start-date
+  :retail-year-end-date
+  :retail-quarter-of-year
+  :retail-quarter-start-date
+  :retail-quarter-end-date
+  :retail-month-of-year
+  :retail-month-of-quarter
+  :retail-month-start-date
+  :retail-month-end-date
+  :retail-week-of-year
+  :retail-week-of-quarter
+  :retail-week-of-month
+  :retail-week-start-date
+  :retail-week-end-date
+  :retail-day-of-year
+  :retail-day-of-quarter
+  :retail-day-of-month
+  :retail-day-of-week
+  "
+  [date & {:keys [calendar-style start-month start-day]
+           :or {calendar-style "454"
+                start-month :february
+                start-day :sunday}}]
+  (let [date
+        (parse-date date)
+
+        calendar-year
+        (jt/as date :year)
+
+        retail-year-cutoff
+        (calc-year-start-date calendar-year start-month start-day)
+
+        retail-year
+        (if (jt/> retail-year-cutoff date)
+          (dec calendar-year)
+          calendar-year)
+
+        retail-year-start-date
+        (calc-year-start-date retail-year start-month start-day)
+
+        retail-year-end-date
+        (-> (inc retail-year)
+            (calc-year-start-date start-month start-day)
+            (jt/minus (jt/days 1)))
+
+        retail-day-of-year* ;; asterisk indicates zero-indexed value
+        (jt/time-between retail-year-start-date date :days)
+
+        retail-week-of-year*
+        (quot retail-day-of-year* 7)
+
+        retail-day-of-week*
+        (as-> retail-week-of-year* %
+          (* % 7)
+          (jt/days %)
+          (jt/plus retail-year-start-date %)
+          (jt/time-between % date :days))
+
+        quarter-start-weeks*
+        [0 13 26 39]
+
+        retail-quarter-of-year*
+        (->> quarter-start-weeks*
+             (filter #(>= retail-week-of-year* %))
+             (count)
+             (dec))
+
+        retail-quarter-start-date
+        (->> retail-quarter-of-year*
+             (* 13 7)
+             (jt/days)
+             (jt/plus retail-year-start-date))
+
+        retail-quarter-end-date
+        (if (= retail-quarter-of-year* 3)
+          retail-year-end-date
+          (->> (inc retail-quarter-of-year*)
+               (* 13 7)
+               (dec)
+               (jt/days)
+               (jt/plus retail-year-start-date)))
+
+        retail-week-of-quarter*
+        (->> retail-quarter-of-year*
+             (* 13)
+             (- retail-week-of-year*))
+
+        month-start-weeks*
+        (condp = calendar-style
+          "454" [0 4 9]
+          "445" [0 4 8]
+          "544" [0 5 9]
+          (throw (IllegalArgumentException.
+                  (str "Invalid calendar style: " calendar-style))))
+
+        retail-month-of-quarter*
+        (->> month-start-weeks*
+             (filter #(>= retail-week-of-quarter* %))
+             (count)
+             (dec))
+
+        retail-month-of-year*
+        (-> retail-quarter-of-year*
+            (* 3)
+            (+ retail-month-of-quarter*))
+
+        retail-month-start-date
+        (->> retail-month-of-quarter*
+             (nth month-start-weeks*)
+             (+ (* retail-quarter-of-year* 13))
+             (jt/weeks)
+             (jt/plus retail-year-start-date))
+
+        retail-month-end-date
+        (if (= retail-month-of-year* 11)
+          retail-year-end-date
+          (if (= retail-month-of-quarter* 2)
+            retail-quarter-end-date
+            (->> (inc retail-month-of-quarter*)
+                 (nth month-start-weeks*)
+                 (+ (* retail-quarter-of-year* 13))
+                 (* 7)
+                 (dec)
+                 (jt/days)
+                 (jt/plus retail-year-start-date))))
+
+        retail-week-of-month*
+        (->> retail-month-of-quarter*
+             (nth month-start-weeks*)
+             (- retail-week-of-quarter*))]
+    {:calendar-date             date
+     :retail-year               retail-year
+     :retail-quarter-of-year    (inc retail-quarter-of-year*)
+     :retail-month-of-year      (inc retail-month-of-year*)
+     :retail-month-of-quarter   (inc retail-month-of-quarter*)
+     :retail-week-of-year       (inc retail-week-of-year*)
+     :retail-week-of-quarter    (inc retail-week-of-quarter*)
+     :retail-week-of-month      (inc retail-week-of-month*)
+     :retail-day-of-year        (inc retail-day-of-year*)
+     :retail-day-of-quarter     (inc (jt/time-between retail-quarter-start-date date :days))
+     :retail-day-of-month       (inc (jt/time-between retail-month-start-date date :days))
+     :retail-day-of-week        (inc retail-day-of-week*)
+     :retail-year-start-date    retail-year-start-date
+     :retail-year-end-date      retail-year-end-date
+     :retail-quarter-start-date retail-quarter-start-date
+     :retail-quarter-end-date   retail-quarter-end-date
+     :retail-month-start-date   retail-month-start-date
+     :retail-month-end-date     retail-month-end-date
+     :retail-week-start-date    (jt/plus retail-year-start-date (jt/weeks retail-week-of-year*))
+     :retail-week-end-date      (jt/plus retail-year-start-date (jt/weeks retail-week-of-year*) (jt/days 6))}))
+
+(defn retail-calendar
+  "Generates a calendar with given start-date, end-date and specified options (or defaults)."
   [start-date end-date & {:keys [calendar-style start-month start-day]
                           :or {calendar-style "454"
                                start-month :february
                                start-day :sunday}}]
-  (let [start-date (parse-date start-date)
-        end-date (parse-date end-date)
-        date-seq (take-while #(jt/<= % end-date)
-                             (jt/iterate jt/plus start-date (jt/days 1)))]
-    (map #(->retail-date % :calendar-style calendar-style :start-month start-month :start-day start-day) date-seq)))
+  (let [start (parse-date start-date)
+        end (parse-date end-date)
+        date-range (take-while #(jt/<= % end)
+                               (jt/iterate jt/plus start (jt/days 1)))]
+    (map #(->retail-date % :calendar-style calendar-style :start-month start-month :start-day start-day) date-range)))
 
-(defn ->retail-dates-ds
-  "Returns a tablecloth dataset of retail dates from a vector of calendar dates."
-  [dates & {:keys [calendar-style start-month start-day]
-            :or {calendar-style "454"
-                 start-month :february
-                 start-day :sunday}}]
-  (->
-   (->retail-dates dates :calendar-style calendar-style :start-month start-month :start-day start-day)
-   (tc/dataset)))
+(defn retail-calendar-dataset
+  "Creates a scicloj.tablecloth dataset of a calendar with given start-date, end-date, and specified options (or defaults)."
+  [start-date end-date & {:keys [calendar-style start-month start-day]
+                          :or {calendar-style "454"
+                               start-month :february
+                               start-day :sunday}}]
+  (-> (retail-calendar start-date end-date :calendar-style calendar-style :start-month start-month :start-day start-day)
+      (tc/dataset)
+      (tc/select-columns [:calendar-date
+                          :retail-year
+                          :retail-quarter-of-year
+                          :retail-month-of-year
+                          :retail-month-of-quarter
+                          :retail-week-of-year
+                          :retail-week-of-quarter
+                          :retail-week-of-month
+                          :retail-day-of-year
+                          :retail-day-of-quarter
+                          :retail-day-of-month
+                          :retail-day-of-week
+                          :retail-year-start-date
+                          :retail-year-end-date
+                          :retail-quarter-start-date
+                          :retail-quarter-end-date
+                          :retail-month-start-date
+                          :retail-month-end-date
+                          :retail-week-start-date
+                          :retail-week-end-date])))
 
-(defn ->retail-date-range-ds
-"Returns a tablecloth dataset of retail dates from a vector of calendar dates."
-  [start-date end-date file-path & {:keys [calendar-style start-month start-day]
+(defn write-retail-calendar!
+  "Writes a retail calendar to the specified path with given start-date, end-date, and specified options (or defaults).
+
+  Filename must end in \".csv\" for comma separated values or \".tsv\" for tab separated values. Optional \".gz\" may
+  be appeneded to the end to create a compressed file."
+  [file-path start-date end-date & {:keys [calendar-style start-month start-day]
                                     :or {calendar-style "454"
                                          start-month :february
                                          start-day :sunday}}]
-  (->
-   (->retail-date-range start-date end-date :calendar-style calendar-style :start-month start-month :start-day start-day)
-   (tc/dataset)))
+  (-> (retail-calendar-dataset start-date end-date :calendar-style calendar-style :start-month start-month :start-day start-day)
+      (tc/write! file-path)))
 
-(defn write-dates!
-  "Writes a vector of retail date maps to disk in the specified format (include either .csv or .tsv in the filename)."
-  [dates file-path & {:keys [calendar-style start-month start-day]
-                      :or {calendar-style "454"
-                           start-month :february
-                           start-day :sunday}}]
-  (->
-   (->retail-dates-ds dates :calendar-style calendar-style :start-month start-month :start-day start-day)
-   (tc/write! file-path)))
+(defn retail-year->calendar
+  "Generates a calendar for a given retail year using specified options (or defaults)."
+  [retail-year & {:keys [calendar-style start-month start-day]
+                  :or {calendar-style "454"
+                       start-month :february
+                       start-day :sunday}}]
+  (let [start-date (calc-year-start-date retail-year start-month start-day)
+        end-date (-> (inc retail-year)
+                     (calc-year-start-date start-month start-day)
+                     (jt/minus (jt/days 1)))]
+    (retail-calendar-dataset start-date end-date :calendar-style calendar-style :start-month start-month :start-day start-day)))
 
-
-(defn write-date-range!
-  "Writes a range of retail dates to disk in the specified format (include either .csv or .tsv in the filename)."
-  [start-date end-date file-path & {:keys [calendar-style start-month start-day]
-                                    :or {calendar-style "454"
-                                         start-month :february
-                                         start-day :sunday}}]
-  (->
-   (->retail-date-range-ds start-date end-date :calendar-style calendar-style :start-month start-month :start-day start-day)
-   (tc/write! file-path)))
+(defn printable-calendar
+  "Creates a printable calendar output for a retail year that mimics the visual format of a standard calendar.
+  The year can be specified with the `:retail-year` option, otherwise defaults to the current system time year."
+  [& {:keys [retail-year calendar-style start-month start-day]
+      :or {retail-year (jt/as (jt/year) :year)
+           calendar-style "454"
+           start-month :february
+           start-day :sunday}}]
+  (-> (retail-year->calendar retail-year :calendar-style calendar-style :start-month start-month :start-day start-day)
+      (tc/add-columns {:day-of-month (fn [x] (map #(jt/as % :day-of-month) (x :calendar-date)))
+                       :weekday (fn [x] (map #(subs (.name (.getDayOfWeek %)) 0 3) (x :calendar-date)))})
+      (tc/group-by [:retail-year :retail-quarter-of-year :retail-month-of-year :retail-week-of-year :weekday])
+      (tc/aggregate-columns [:day-of-month] #(reduce min %))
+      (tc/crosstab [:retail-year :retail-quarter-of-year :retail-month-of-year :retail-week-of-year]
+                   [:weekday]
+                   {:aggregator #(reduce min (:day-of-month %))})))
